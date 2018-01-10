bd2f781c437e2cb447a00b95fe9754f1365e54ca
Christopher Lewis <lewsid@lewsid.com>
2018-01-10 17:43:27 -0500

    minor messaging tweak

--- basic_admin/apps/admin/layout/messaging.php
+++ basic_admin/apps/admin/layout/messaging.php
@@ -1,22 +1,22 @@
 <?php if(DinklyFlash::exists('good_user_message')): ?>
 	<div class="alert alert-success">
-		<?php echo DinklyFlash::get('good_user_message'); ?>
 		<button type="button" class="close message-close" aria-hidden="true">&times;</button>
+		<?php echo DinklyFlash::get('good_user_message'); ?>
 	</div>
 <?php endif; ?>
 <?php if(DinklyFlash::exists('error')): ?>
 	<div class="alert alert-danger">
-		<?php echo DinklyFlash::get('error'); ?>
 		<button type="button" class="close message-close" aria-hidden="true">&times;</button>
+		<?php echo DinklyFlash::get('error'); ?>
 	</div>
 <?php endif; ?>
 <?php if(DinklyFlash::exists('errors')): ?>
     <div class="alert alert-danger">
+    	<button type="button" class="close message-close" aria-hidden="true">&times;</button>
         <ul>
             <?php foreach(DinklyFlash::get('errors') as $error): ?>
                 <li><?php echo $error; ?></li>
             <?php endforeach; ?>
         </ul>
-        <button type="button" class="close message-close" aria-hidden="true">&times;</button>
     </div>
 <?php endif; ?>
